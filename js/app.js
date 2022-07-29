41b49474496fecb1eab7063164f6ba0f2c27dce4
Scurrlin <sean.currlin@gmail.com>
2022-07-29 09:08:55 -0400

    Fixed bug in playInterval funcction

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ let correctAnswers = 0;
 let questionCount = 10;
 let beginTime = "";
 let lastId;
-let startId;
+
 
 let playInterval = function( id ){
    if(lastId){
@@ -13,9 +13,6 @@ let playInterval = function( id ){
    }
    document.getElementById(id).play();
    lastId = id
-   if(startId){
-   document.getElementById(startId).pause();
-   }
 }
 
 let generateTest = function(){
@@ -29,9 +26,9 @@ let generateTest = function(){
 
     for( let question = 0; question < questionCount; question++ )
     {
-        let interval = getRandomInterval();
-        questions[question] = interval.code + getRandomIndex();
-        answers[question] = interval;
+      let interval = getRandomInterval();
+      questions[question] = interval.code + getRandomIndex();
+      answers[question] = interval;
     }
 
     beginTime = Date.now();
@@ -40,142 +37,140 @@ let generateTest = function(){
 
 let nextQuestion = function()
 {
-    show("questionIndex", "Step " + ( questionIndex + 1 ) + " of " + questionCount );
-    playInterval( questions[questionIndex] );
+  show("questionIndex", "Step " + ( questionIndex + 1 ) + " of " + questionCount );
+  playInterval( questions[questionIndex] );
 }
 
 let confirmAnswer = function()
 {
-    evaluateAnswer();
-    questionIndex++;
-    if( questionIndex < questionCount )
-    {
-        nextQuestion();
-    }
-    else
-    {
-        finishGame();
-    }
+  evaluateAnswer();
+  questionIndex++;
+  if( questionIndex < questionCount )
+  {
+    nextQuestion();
+  }
+  else
+  {
+    finishGame();
+  }
 }
 
 let evaluateAnswer = function() {
 
-    let id = document.getElementById("answer").value;
-    let interval = getInterval( id );
-    if( interval.code === answers[questionIndex].code)
-    {
-        correctAnswers++;
-        show("message", "&nbsp");
-    }
-    else
-    {
-        show("message", "Nay! The correct answer was: " + answers[questionIndex].name );
-    }
+  let id = document.getElementById("answer").value;
+  let interval = getInterval( id );
+  if( interval.code === answers[questionIndex].code)
+  {
+    correctAnswers++;
+    show("message", "&nbsp");
+  }
+  else
+  {
+    show("message", "Nay! The correct answer was: " + answers[questionIndex].name );
+  }
 }
 
 let finishGame = function()
 {
-    show("startButton");
-    hide("questionArea");
-    let highScoreMessage = "You didn't beat your high score. ";
-    let delta = Date.now() - beginTime; // milliseconds elapsed since start
-    let completionSeconds = Math.floor(delta / 1000);
-    if( saveHighScore( completionSeconds ) )
-    {
-        highScoreMessage = "New high score! ";
-    }
+  show("startButton");
+  hide("questionArea");
+  let highScoreMessage = "You didn't beat your high score. ";
+  let delta = Date.now() - beginTime;
+  let completionSeconds = Math.floor(delta / 1000);
+  if( saveHighScore( completionSeconds ) )
+  {
+    highScoreMessage = "New high score! ";
+  }
 
-    show( "message", highScoreMessage +
-                         "You got " + correctAnswers +
-                         " out of " + questionCount +
-                         " correct in " + completionSeconds + " seconds." );
+  show( "message", highScoreMessage + "You got " + correctAnswers +
+    " out of " + questionCount + " correct in " + completionSeconds + " seconds." );
 }
 
 let saveHighScore = function( completionSeconds )
 {
-    let highScore = false;
-    let score = localStorage.getItem('score');
-    let time = localStorage.getItem('time');
-    if( correctAnswers > score || ( correctAnswers >= score && completionSeconds < time ) )
-    {
-        localStorage.setItem('score', correctAnswers);
-        localStorage.setItem('time', completionSeconds);
-        highScore = true;
-    }
-    return highScore;
+  let highScore = false;
+  let score = localStorage.getItem('score');
+  let time = localStorage.getItem('time');
+  if( correctAnswers > score || ( correctAnswers >= score && completionSeconds < time ) )
+  {
+    localStorage.setItem('score', correctAnswers);
+    localStorage.setItem('time', completionSeconds);
+    highScore = true;
+  }
+  return highScore;
 }
 
 
 let replayInterval = function()
 {
-    playInterval( questions[questionIndex] );
+  playInterval( questions[questionIndex] );
 }
 
 let getRandomInterval = function()
 {
-    let interval = getRandomInteger( 0, 9 );
-    return getInterval( interval );
+  let interval = getRandomInteger( 0, 9 );
+  return getInterval( interval );
 }
 
 let show = function( id, value )
 {
-    let element = document.getElementById(id);
-    element.style.display = "";
-    if( value !== undefined )
-    {
-        element.innerHTML = value;
-    }
+  let element = document.getElementById(id);
+  element.style.display = "";
+  if( value !== undefined )
+  {
+    element.innerHTML = value;
+  }
 }
 
 let hide = function( id )
 {
-    document.getElementById(id).style.display = "none";
+  document.getElementById(id).style.display = "none";
 }
 
 let getInterval = function( id )
 {
-    startId = id
-    id = id + "";
-    let interval = { code: "Msecond", name: "Major Second" }
-    switch( id )
-    {
-      case "0":
-        interval = { code: "Msecond", name: "Major Second" };
-        break;
-      case "1":
-        interval = { code: "msecond", name: "Minor Second"}
-        break;
-      case "2":
-        interval = { code: "Mthird", name: "Major Third" };
-        break;
-      case "3":
-        interval = { code: "mthird", name: "Minor Third"}
-        break;
-      case "4":
-        interval = { code: "fourth", name: "Perfect Fourth" }
-        break;
-      case "5":
-        interval = { code: "tritone", name: "Tritone" }
-        break;
-      case "6":
-        interval = { code: "fifth", name: "Perfect Fifth" }
-        break;
-      case "7":
-        interval = { code: "Msixth", name: "Major Sixth" };
-        break;
-      case "8":
-        interval = { code: "msixth", name: "Minor Sixth"}
-        break;
-      case "9":
-        interval = { code: "Mseventh", name: "Major Seventh" }
-        break;
-      case "10":
-        interval = { code: "mseventh", name: "Minor Seventh" }
-        break;
-      case "11":
-        interval = { code: "octave", name: "Octave" }
-        break;
-      default:
+  startId = id
+  id = id + "";
+  let interval = { code: "Msecond", name: "Major Second" }
+  switch( id )
+  {
+    case "0":
+      interval = { code: "Msecond", name: "Major Second" };
+      break;
+    case "1":
+      interval = { code: "msecond", name: "Minor Second"}
+      break;
+    case "2":
+      interval = { code: "Mthird", name: "Major Third" };
+      break;
+    case "3":
+      interval = { code: "mthird", name: "Minor Third"}
+      break;
+    case "4":
+      interval = { code: "fourth", name: "Perfect Fourth" }
+      break;
+    case "5":
+      interval = { code: "tritone", name: "Tritone" }
+      break;
+    case "6":
+      interval = { code: "fifth", name: "Perfect Fifth" }
+      break;
+    case "7":
+      interval = { code: "Msixth", name: "Major Sixth" };
+      break;
+    case "8":
+      interval = { code: "msixth", name: "Minor Sixth"}
+      break;
+    case "9":
+      interval = { code: "Mseventh", name: "Major Seventh" }
+      break;
+    case "10":
+      interval = { code: "mseventh", name: "Minor Seventh" }
+      break;
+    case "11":
+      interval = { code: "octave", name: "Octave" }
+      break;
+    default:
         
     }
 
